7843f5220c6a82cacc21ddd2f3523eeba9ee18c4
David <wtih123@gmail.com>
2021-05-18 22:20:24 -0300

    Main tela Inicial => para revisão

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,69 +1,64 @@
 <template>
-  <q-layout view="hHh Lpr fFf">
-    <div class="grid">
-        <header class="header">
+  <q-layout view="hHh Lpr fFf" class="grid">
+    <nav class="grid">
+        <ul class="header">
+          <p class="marca"><a href="">Elegant kids</a></p>
           <div class="header-content">
-           <p class="marca">Elegant kids</p>
-           <p>Como funcionamos</p>
-           <p>Vantangens</p>
-           <p>Perguntas Frequentes</p>
+           <li><a href="">Como funcionamos</a></li>
+           <li><a href="">Vantangens</a></li>
+           <li><a href="">Perguntas Frequentes</a></li>
            <button>Cadastro</button>
            <a href="#">Login</a>
           </div>
-        </header>
-    </div>
+        </ul>
+          <section class="main">
+            <h3>O Melhor da moda infantil na sua casa</h3>
+            <div class="facilidade">
+              <img src=".././assets/bolsinha.png" />
+            </div>
+            <div  class="PowerBag">
+              <p>Power bag</p>
+            </div>
+            <div  class="facilidade">
+              <p>Compre sem sair de casa</p>
+            </div>
+            <div clas="steps">
+              <p class="logos">Receba</p>
+              <p class="logos">Prove</p>
+              <p class="logos">Pague</p>
+            </div>
+            <div class="logo">
+                <img src=".././assets/carrinho.png" alt="Carrinho"/>
+                <img src=".././assets/roupa-roxa.png" alt="Roupa"/>
+                <img src=".././assets/cartao.png" alt="Cartao"/>
+            </div>
+          </section>
 
-    <div class="title">
-        <section class="main">
-          <h3>O Melhor da moda infantil na sua casa</h3>
-          <div class="logo">
-            <div>
-              <p class="logo-1">Receba</p>
-            </div>
-            <div>
-               <p class="logo-2">Prove</p>
-            </div>
-            <div>
-              <p class="logo-3">Pague</p>
-            </div>
-          </div>
-        </section>
-        <section class="aside">
-          <h3>Como funcionamos?</h3>
-          <p class="region">entregamos para todo o Brasil</p>
-          <div class="grid-content">
-            <div class="icol">
-              <div class="content">
-                <p>1. Preencha algumas perguntas para conhecermos suas necessidades.</p>
+          <section class="aside">
+            <h3>Como funcionamos?</h3>
+            <p class="regiao">entregamos para todo o Brasil</p>
+            <div class="grid-content">
+              <div class="grid-icone">
+                <p><img src=".././assets/lista.png" alt="Cartao"/>
+                1.Preencha algumas perguntas para conhecermos suas necessidades.</p>
+              </div>
+              <div class="grid-icone">
+                <p><img src=".././assets/coment.png" alt="Cartao">
+                  2. Após o preenchimento das perguntas entramos em contato via WhatsApp, para confirmar as peças/estilo que gostou e agendamos a entrega da sua Bag.
+                </p>
+              </div>
+              <div class="grid-icone">
+                <p><img src=".././assets/user-check.png" alt="Cartao"/>
+                  3.Prontinho! Sua bag personalizada será entregue em até 24horas (após a aprovação do seu pedido).</p>
               </div>
             </div>
             <div class="icol">
-              <div class="content">
-                <p>2. Após o preenchimento das perguntas entramos em contato via WhatsApp, para confirmar as peças/estilo que gostou e agendamos a entrega da sua Bag.</p>
-              </div>
+              <button >Eu quero uma bag</button>
+              <img src=".././assets/bag.png" class="icol2" alt="bag"/>
             </div>
-            <div class="icol">
-              <div class="content">
-                <p>3. Prontinho! Sua bag personalizada será entregue em até 24horas (após a aprovação do seu pedido).</p>
-              </div>
-            </div>
-          </div>
+          </section>
 
-          <button>Eu quero uma bag</button>
-          <div class="icol"></div>
-      </section>
-    </div>
-
-    <div class="row">
-      <q-footer>
-        <q-toolbar class="">
-          <q-toolbar-title class="">PowerBag</q-toolbar-title>
-          <p class="text-caption absolute-center">
-            Todos Direitos Reservados, Bags PowerBag
-          </p>
-        </q-toolbar>
-      </q-footer>
-    </div>
+    </nav>
   </q-layout>
 </template>
 
@@ -76,6 +71,7 @@
   text-decoration: none;
   box-sizing: border-box;
   font-family: 'Rubik', sans-serif;
+  list-style: none;
 }
 
 .grid {
@@ -85,42 +81,158 @@
   background-color: #507CFF;
 }
 
-.header-content{
+.header-content {
   display: flex;
   align-content: center;
   justify-content: center;
-  margin: 25px;
 }
 
-.header-content p, a{
-  margin: 20px;
+.marca {
+ font-size: 25px;
+ color: #F7F4F3;
+ margin: 0 10px 0 25px;
+ position: relative;
+ top: 40px
+}
+
+.header-content li, a{
+  margin: 10px 10px 30px 10px;
   color: #F7F4F3;
   cursor: pointer;
+  padding: 5px;
 }
+
 
 .header-content button {
   background-color: #111111;
   color: #F7F4F3;
-  border: 5px;
-  padding: 10px;
+  padding: 8px;
   cursor: pointer;
+  width: 111px;
+  height: 45px;
+  border-radius: 8px;
+  margin-left: 150px;
 }
 
-.main h3, p {
+.main {
+  width: 100%;
+  margin-bottom: 50px;
+  height: 310px;
+}
+
+.main h3 {
+  color:#000000;
+  text-align: center;
+  margin: 40px
+}
+
+.PowerBag {
+  font-size: 20px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 24px;
+}
+
+.facilidade {
+  font-family: Rubik;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 10px;
+  line-height: 12px;
   color: #000000;
+}
+
+.main div {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.steps {
+  display: block;
+}
+.logos {
+  margin-top: 25px;
+  margin-left: 25px;
+  margin-right: 25px;
+}
+
+.main p + p::before{
+  content: '';
+  position: absolute;
+  width: 20px;
+  height: 2px;
+  left: 625px;
+  background: #000000;
+  top: 354px
+}
+
+.main p + p + p::before{
+  content: '';
+  position: absolute;
+  width: 20px;
+  height: 2px;
+  left: 710px;
+  background: #000000;
+  top: 354px
+}
+
+.logo img {
+  margin-top: 25px;
+  margin-left: 25px;
+  margin-right: 25px;
 }
 
 .aside {
   background: #111111;
+  width: 1348px;
+  height: 400px;
 }
 
 .aside h3 {
   color: #F7F4F3;
+  text-align: center;
+  margin: 20px;
 }
 
-.aside p, div {
+.regiao {
   color: #F7F4F3;
+  text-align: center;
+  margin: 20px;
 }
+
+.grid-content {
+  display: flex;
+  margin: 10px;
+  align-items: center;
+}
+
+.grid-icone {
+  margin: 30px;
+}
+
+.grid-icone p{
+ color: #F7F4F3;
+}
+.icol {
+  align-content: center;
+  align-items: center;
+  position: relative;
+  left: 550px;
+}
+
+.icol button {
+ color: #000000;
+ width: 141px;
+ height: 55px;
+ padding: 5px;
+ border-radius: 8px;
+ cursor: pointer;
+ margin: 0 10px;
+}
+
+
 </style>
 
 
