9ab37974edfa6964537804e161df10509fc32049
Ygor Mattos <ygormattos.b@gmail.com>
2021-05-21 12:39:16 -0300

    update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const
   express = require('express'),
   serveStatic = require('serve-static'),
-  history = require('connect-history-api-fallback'),
+  // history = require('connect-history-api-fallback'),
   path = require('path'),
   port = process.env.PORT || 5000
 
 const app = express()
 
-app.use(history())
+// app.use(history())
 app.use(serveStatic(path.join(__dirname, 'dist', 'spa')))
 app.listen(port)
 console.log('Server started ' + port);
