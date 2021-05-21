82f40104e72555de28fb3416581eef49a57f235f
Ygor Mattos <ygormattos.b@gmail.com>
2021-05-21 01:35:07 -0300

    deploy

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import express from 'express'
-// import serveStatic from 'serve-static'
-import history from 'connect-history-api-fallback'
-
-port = process.env.PORT || 50
+const
+  express = require('express'),
+  serveStatic = require('serve-static'),
+  history = require('connect-history-api-fallback'),
+  port = process.env.PORT || 5000
 
 const app = express()
 
 app.use(history())
-app.use(express.static(__dirname + '/dist/spa'))
+app.use(serveStatic(__dirname + '/dist/spa'))
 app.listen(port)
