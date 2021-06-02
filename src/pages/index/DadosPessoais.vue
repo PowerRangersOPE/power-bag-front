a3a5c7b9129346097c6a09362adf64ac9fe9251a
ricardowoj <ricardowoj@hotmail.com>
2021-06-01 22:15:55 -0300

    refatorado validacao dados pessoais

--- src/pages/index/DadosPessoais.vue
+++ src/pages/index/DadosPessoais.vue
@@ -24,18 +24,21 @@
     <div class="row q-mt-lg">
       <div class="col-12 col-sm">
         <q-input
+          ref="email"
           v-model="cadastroPerfil.email"
           class="q-mr-lg"
           label="E-mail"
+          :rules="[this.required, this.short, this.isEmail]"
         />
       </div>
       <div class="col-12 col-sm">
         <q-input
+          ref="dat_nasc"
           v-model="cadastroPerfil.dat_nasc"
           class=""
           label="Data de nascimento"
           mask="##/##/####"
-          :rules="[this.isDataVencimento]"
+          :rules="[this.required, this.isDateNasc]"
         />
       </div>
     </div>
@@ -43,17 +46,21 @@
     <div class="row q-mt-lg">
       <div class="col-12 col-sm">
         <q-input
+          ref="tel_cel1"
           v-model="cadastroPerfil.tel_cel1"
           class="q-mr-lg"
           label="Celular principal"
           mask="(##) ##### - ####"
+          :rules="[this.required, this.isCellPhone]"
         />
       </div>
       <div class="col-12 col-sm">
         <q-input
+          ref="tel_cel2"
           v-model="cadastroPerfil.tel_cel2"
-          label="Celular secundário"
+          label="Telefone secundário"
           mask="(##) ##### - ####"
+          :rules="[this.required, this.isCellPhone]"
         />
       </div>
     </div>
@@ -113,51 +120,74 @@ export default {
     this.buscardados();
   },
   methods: {
-    isDataVencimento(data) {
-      data = data.replace(/\//g, "-"); // substitui eventuais barras (ex. IE) "/" por hífen "-"
-      var data_array = data.split("-"); // quebra a data em array
-
-      // para o IE onde será inserido no formato dd/MM/yyyy
-      if (data_array[0].length != 4) {
-        data = data_array[2] + "-" + data_array[1] + "-" + data_array[0]; // remonto a data no formato yyyy/MM/dd
-      }
-
-      // comparo as datas e calculo a idade
-      var hoje = new Date();
-      var nasc = new Date(data);
-      var idade = hoje.getFullYear() - nasc.getFullYear();
-      var m = hoje.getMonth() - nasc.getMonth();
-      if (m < 0 || (m === 0 && hoje.getDate() < nasc.getDate())) idade--;
-
-      if (idade < 18) {
-        return ("Data inválida");
-      }
-
-      if (idade >= 90) {
-        return ("Data inválida");
-      }
+    required(val) {
+      return (val && val.length > 0) || "O campo deve ser preenchido";
+    },
+    short(val) {
+      return (val && val.length > 3) || "Mínimo 4 caracters";
+    },
+    isEmail(val) {
+      const emailPattern = /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/;
+      return emailPattern.test(val) || "Por favor digite e-mail válido";
+    },
+    isCellPhone(val) {
+      const cellPattern = /^(?:(?:\+|00)?(55)\s?)?(?:(?:\(?[1-9][0-9]\)?)?\s?)?(?:((?:9\d|[2-9])\d{3})-?(\d{4}))$/;
+      var er = /[^a-z0-9]/gi;
+      return cellPattern.test(val.replace(er, "")) || "Por favor digite celular válido"
+    },
+    isDateNasc(val) {
+      const datePattern = /^(?:(?:31(\/)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(\/)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+      return datePattern.test(val) || "Por favor digite data válida"
     },
     confirmaSalvar() {
       this.confirm = true;
     },
     salvarDadosPessoais() {
-      axios({
-        method: "PUT",
-        url: `${this.baseUrl}/cliente/${this.clienteId}`,
-        data: {
-          email: this.cadastroPerfil.email,
-          tel_cel1: this.cadastroPerfil.tel_cel1,
-          tel_cel2: this.cadastroPerfil.tel_cel2,
-          dat_nasc: this.cadastroPerfil.dat_nasc
-        }
-      });
-      setTimeout(() => {
+      this.$q.loading.show()
+      this.$refs.email.validate()
+      this.$refs.dat_nasc.validate()
+      this.$refs.tel_cel1.validate()
+      this.$refs.tel_cel2.validate()
+
+      if(
+       this.$refs.email.hasError || 
+       this.$refs.tel_cel1.hasError || 
+       this.$refs.tel_cel2.hasError ||
+       this.$refs.dat_nasc.hasError 
+       ) {
         this.$q.dialog({
-          title: "Parabéns!",
-          message: "Dados salvo com sucesso!"
+          title: 'Atenção',
+          message: 'Dados preenchidos incorretamente.'
+        })
+        this.$q.loading.hide()
+        return
+      }
+      try {
+        axios({
+          method: "PUT",
+          url: `${this.baseUrl}/cliente/${this.clienteId}`,
+          data: {
+            email: this.cadastroPerfil.email,
+            tel_cel1: this.cadastroPerfil.tel_cel1,
+            tel_cel2: this.cadastroPerfil.tel_cel2,
+            dat_nasc: this.cadastroPerfil.dat_nasc
+          }
         });
-        this.buscardados();
-      }, 500);
+        setTimeout(() => {
+          this.$q.dialog({
+            title: "Parabéns!",
+            message: "Dados salvo com sucesso!"
+          });
+          this.$q.loading.hide()
+          this.buscardados();
+        }, 500);
+      } catch (error) {
+        this.$q.dialog({
+          title: 'Atenção',
+          message: 'Erro ao tentar salvar. Entre em contato com Administrador'
+        })
+        this.$q.loading.hide()
+      }
     },
     async buscardados() {
       this.cadastroPerfil.nome = localStorage.getItem("nome");
